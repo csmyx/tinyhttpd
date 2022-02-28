44973c3840c1df5b14e757b10ba44c101c5bea68
myx <ustcmyx@163.com>
2022-02-28 15:03:29 +0800

    version 1

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-all: httpd
+all: httpd client
 
 httpd: httpd.c
-	gcc -W -Wall -lpthread -o httpd httpd.c
+	gcc -g -W -Wall -lpthread -o httpd httpd.c
+
+client: client.c
+	gcc -g -W -Wall -o $@ $^
 
 clean:
 	rm httpd
+	rm client
--- client.c
+++ client.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+
+#define DEBUG_ 1
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +15,16 @@ int main(int argc, char *argv[])
  int len;
  struct sockaddr_in address;
  int result;
- char ch = 'A';
+
+ short server_port = 8080;
+ char *url = "/b.cgi";
+ if (argc == 3) {
+   short server_port = (short)atoi(argv[1]); 
+   char *url = argv[2];
+ } else if (!DEBUG_) {
+   printf("please input SERVER_PORT and URL!\n");
+   exit(1);
+ }
 
  //申请一个流 socket
  sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -19,8 +32,8 @@ int main(int argc, char *argv[])
  address.sin_family = AF_INET;
  //inet_addr 可以参考 man inet_addr
  //可以用现代的inet_pton()替代inet_addr(), example 中有参考例子
- address.sin_addr.s_addr = inet_addr("127.0.0.1");
- address.sin_port = htons(9734);
+ address.sin_addr.s_addr = inet_addr("124.221.128.53");
+ address.sin_port = htons(server_port);
  len = sizeof(address);
  
  //下面的语句可以输出连接的 IP 地址
@@ -35,11 +48,16 @@ int main(int argc, char *argv[])
   exit(1);
  }
  
- //往服务端写一个字节
- write(sockfd, &ch, 1);
- //从服务端读一个字符
- read(sockfd, &ch, 1);
- printf("char from server = %c\n", ch);
+ char buf[1024];
+ sprintf(buf, "GET %s\r\n\r\n", url);
+ // send to server 
+ write(sockfd, buf, strlen(buf));
+
+ printf("recv form server:\n");
+ int sz = 0;
+ while ((sz = read(sockfd, buf, sizeof(buf))) != 0) {
+  write(1, buf, sz);
+ }
  close(sockfd);
  exit(0);
 }
